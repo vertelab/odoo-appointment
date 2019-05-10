7720942008a145da83d18f679290f3367ab73d9d
Jakob <jakob_barsomo97@hotmail.se>
2019-05-10 16:57:05 +0200

    Calendar_appointment : Extended work on wizard

--- calendar_appointment/calendar.py
+++ calendar_appointment/calendar.py
@@ -2,6 +2,7 @@ import odoo
 from odoo import api, fields, models, _
 from odoo.exceptions import UserError
 import logging
+from datetime import timedelta, time
 
 
 _logger = logging.getLogger(__name__)
@@ -16,7 +17,7 @@ class calendar_appointment(models.Model):
 	attendee_ids = fields.Many2many(comodel_name ='res.partner') # Comodel_name 
 	# ~ product_ids = fields.Many2many(comodel_name = 'product.product')
 	spot_ids = fields.One2many(comodel_name = 'calendar.appointment.spot', inverse_name = 'appointment_id')
-	user_id = fields.Many2one(comodel_name = 'res.user')
+	user_id = fields.Many2one(comodel_name = 'res.users')
 	is_published = fields.Boolean(string = 'Publish') # String represents a label
 	description = fields.Text(string = 'Descriptions')
 	meeting_type = fields.Selection([('One2one', 'One to One'),('Many2one', 'Many to One'),], 'Type Selections') # Ena värdet visa i dropdown och andra lagras i DB:n
@@ -36,7 +37,7 @@ class calendar_appointment_spot(models.Model):
     note = fields.Text(string='Note field')
     date_start = fields.Datetime(string = 'Date start')
     duration = fields.Float(string = 'Duration')
-	# ~ date_end = 
+    date_end = fields.Datetime(string = 'Date End')
 	
 class calendar_event(models.Model):
 	
@@ -45,13 +46,53 @@ class calendar_event(models.Model):
 	
 	appointment_id = fields.Many2one(comodel_name='calendar.appointment')
 	
-	
+	# ~ På en appointment, lägg till spots. Wizard: Välj antal, längd, när på dagen för första spot och antal spots per dag.
 class Wizard(models.TransientModel):
-	_name = 'calendar.wizard'
+	_name = 'calendar.appointment.wizard'
 	_description = 'Wizard for adding spots'
 	
-	def _default_session(self):
-		return self.env['calendar.session'].browse(self._context.get('active_id'))
+	def _default_appointment(self):
+		return self.env['calendar.appointment'].browse(self._context.get('active_id'))
 	
-	session_id = fields.Many2one('calendar.session', string='Session', required=True)
-	attendee_ids = fields.Many2many('res.partner', string='Attendees')
+	date_start = fields.Datetime(string='Start Date')
+	date_stop = fields.Datetime(string='Stop Date')
+	duration = fields.Float(string='Duration')
+	nmbr_spots = fields.Integer(string='Number of Spots')
+	nmbr_spots_per_day = fields.Integer(string='Number of spots per day')
+	
+	@api.multi
+	def create_spots(self):
+		new_day = 0
+		spots_i = 1
+		
+		spot_ids = []
+		while spots_i <= self.nmbr_spots:
+			spots_per_day_i = 1
+			while spots_per_day_i <= self.nmbr_spots_per_day and spots_i <= self.nmbr_spots:
+				
+				appointment = self._default_appointment()
+				event_list = self.env['calendar.event'].search([('start_date','>=',self.date_start), ('stop_date', '<=',self.date_stop), ('user_id', '=', appointment.user_id.id)])
+				# ~ event_list_item = event_list[spots_per_day_i-1]
+				
+				spot_datetime = self.date_start + timedelta(hours=((spots_per_day_i - 1) * self.duration), days=new_day)
+				# ~ _logger.warn(spot_datetime)
+				
+				if event_list:
+					_logger.warn("THIS IS SPARTA")
+					
+				spot_ids.append(self.env['calendar.appointment.spot'].create({
+				'date_start' : spot_datetime,
+				'date_end' : self.date_stop,
+				'appointment_id' : self._context.get('active_id'),
+				'duration' : self.duration}).id)
+			
+				spots_per_day_i += 1
+				spots_i += 1
+				
+			new_day += 1
+		action = self.env['ir.actions.act_window'].for_xml_id('calendar_appointment', 'spot_menu_action')
+		action['domain'] = [('id', 'in', spot_ids)]
+		# ~ _logger.warn(action)
+		
+		_logger.warn(event_list)
+		return action
